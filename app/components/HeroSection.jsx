b779804ac83fe090c8a47c2ecaa124d0c48fea0f
Tae <taekyu.lee3@gmail.com>
2024-02-08 18:29:12 +1100

    navbar complete

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -8,11 +8,11 @@ const HeroSection = () => {
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl front-extrabold">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
               Hello, I'm{" "}
             </span>
-            <br></br>
+            <br />
             <TypeAnimation
               sequence={[
                 "Tae",
