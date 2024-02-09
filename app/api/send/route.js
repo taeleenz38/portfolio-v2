aff90afc86431ec193deeacd9946fcf569db4f21
Tae <taekyu.lee3@gmail.com>
2024-02-09 15:07:04 +1100

    PORTFOLIO ALMOST DONE

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,4 +1,3 @@
-// import { EmailTemplate } from '../../../components/EmailTemplate';
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
@@ -6,26 +5,24 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
-    const { body } = await req.json();
-    const { email, subject, message } = body;
-
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
       from: fromEmail,
-      to: ["taekyu.lee3@gmail.com", email],
+      to: [fromEmail, email],
       subject: subject,
       react: (
         <>
           <h1>{subject}</h1>
-          <p>Thank you for contacting me!</p>
+          <p>Thank you for contacting us!</p>
           <p>New message submitted:</p>
           <p>{message}</p>
         </>
       ),
     });
-
-    return Response.json(data);
+    return NextResponse.json(data);
   } catch (error) {
-    return Response.json({ error });
+    return NextResponse.json({ error });
   }
 }
