1e836072a429162d3ddd2a87c73e0e35e197cdc8
Tae <taekyu.lee3@gmail.com>
2024-02-09 15:21:08 +1100

    ready for deployment

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -55,14 +55,14 @@ const AboutSection = () => {
   return (
     <section className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about.webp" className="rounded-full" width={500} height={500} alt={about} />
+        <Image src="/images/about.webp" className="rounded-full" width={500} height={500} alt="about" />
         <div className="mt-8 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-md ">
             Highly motivated junior developer with a love for front end design.
             Transitioned into coding after dabbling in the web 3.0 space and
             audio production, sparking my interest when I tried to build a
-            marketplace (website) to upload my instrumentals/beats.
+            marketplace to upload my instrumentals/beats.
           </p>
           <div className="flex flex-row mt-8 md:text-2xl">
             <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}> Skills </TabButton>
